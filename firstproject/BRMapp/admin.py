27ec1c4b85ae1f4fb0e90772d9b3feaf7f48660e
Nirupma <94184582+nirupma2208@users.noreply.github.com>
2022-01-19 06:51:49 +0530

    first commit

---
+++ firstproject/BRMapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from BRMapp.models import BRMuser
+
+admin.site.register(BRMuser)
+
+# Register your models here.
---
+++ firstproject/BRMapp/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from BRMapp import views
+
+urlpatterns=[
+    url('view-books',views.viewBooks),
+    url('edit-book',views.editBook),
+    url('delete-book',views.deleteBook),
+    url('search-book',views.searchBook),
+    url('new-book',views.newBook),
+    url(r'^add',views.add),
+    url('search',views.search),
+    url('edit',views.edit),
+    url('login',views.userLogin),
+    url('logout',views.userLogout),
+    # url('view')
+]
